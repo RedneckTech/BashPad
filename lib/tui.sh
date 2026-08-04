934156768ddc804ee476ec3c49beab82d8cb00ca
Jacob Pfeiff <pfeiff33@gmail.com>
2026-08-04 05:04:29 -0500

    started work on TUI

--- lib/tui.sh
+++ lib/tui.sh
@@ -4,3 +4,13 @@ fatal() {
     echo '[fatal]' "$@" >&2
     exit 1
 }
+
+window() {
+    local win_width win_height
+    read -r win_height win_width < <(stty size 2>/dev/null || echo "0 80")
+    echo "$win_width, $win_height"
+    gum style \
+        --foreground 212 --border-foreground 210 --border double \
+        --align center --width "$((win_width - 2))" --height "$((win_height - 2))" \
+        'hello world' 'this is a test'
+}
